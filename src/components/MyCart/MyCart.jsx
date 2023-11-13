f673bed9b490b092646be0698709eaf79e8102f8
RajMahammadAli <ali.rc.raj@gmail.com>
2023-11-13 15:16:19 +0600

    my cart is connected with mongodb

--- src/components/MyCart/MyCart.jsx
+++ src/components/MyCart/MyCart.jsx
@@ -56,6 +56,35 @@ export default function MyCartPage() {
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-3xl font-semibold mb-4">My Cart</h1>
+      {carts.length === 0 ? (
+        <p>Your cart is empty.</p>
+      ) : (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          {carts.map((product, idx) => (
+            <div key={idx} className="bg-white shadow-lg rounded-lg">
+              <img
+                src={product.image}
+                alt={product.name}
+                className="w-full h-40 object-cover rounded-t-lg"
+              />
+              <div className="p-4">
+                <h2 className="text-xl font-semibold">{product.name}</h2>
+                <p className="text-gray-500">{product.brand}</p>
+                <p className="text-gray-700">${product.price}</p>
+                <p className="text-yellow-400">
+                  {/* You can implement a star rating component here */}
+                  Rating: {product.rating}
+                </p>
+                <button
+                  onClick={() => handleDelete(product._id)}
+                  className="mt-2 bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-full focus:outline-none"
+                >
+                  Remove from Cart
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
       )}
     </div>
   );

2f2586075ada981a89266b8cc25729802f3477a6
RajMahammadAli <ali.rc.raj@gmail.com>
2023-11-13 15:11:45 +0600

    register page is ready password is ready as requirement

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -39,7 +39,48 @@ export default function () {
   return (
     <>
       <div>
-        <div className="hero min-h-screen bg-base-200"></div>
+        <div className="hero min-h-screen bg-base-200">
+          <div className="hero-content flex-col lg:flex-row-reverse">
+            <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+              <form onSubmit={handleCreateUser} className="card-body">
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Email</span>
+                  </label>
+                  <input
+                    type="email"
+                    placeholder="email"
+                    className="input input-bordered"
+                    name="email"
+                    required
+                  />
+                </div>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Password</span>
+                  </label>
+                  <input
+                    type="password"
+                    placeholder="password"
+                    className="input input-bordered"
+                    name="password"
+                    required
+                  />
+                </div>
+                <div>
+                  <p className="text-red-700">{error}</p>
+                </div>
+                <div className="form-control mt-6">
+                  <button className="btn btn-primary">Register</button>
+                </div>
+                <label className="label">
+                  Already Registered?
+                  <Link to="/login">Log In</Link>
+                </label>
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );

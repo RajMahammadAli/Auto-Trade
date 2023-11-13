b6d7690c5863c43c48809b0d7af84025d3f8f0c3
RajMahammadAli <ali.rc.raj@gmail.com>
2023-11-13 15:14:32 +0600

    update form is connected with mongodb

--- src/components/UpdataForm/UpdateForm.jsx
+++ src/components/UpdataForm/UpdateForm.jsx
@@ -60,7 +60,100 @@ export default function () {
     <>
       <div className="container mx-auto mt-8 mb-8">
         <h2 className="text-3xl font-semibold mb-4">Update Product</h2>
-
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label htmlFor="image" className="block text-gray-600">
+              Image URL
+            </label>
+            <input
+              type="text"
+              id="image"
+              name="image"
+              className="w-full border border-gray-300 rounded-md py-2 px-3"
+              value={updatedProduct.image}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="name" className="block text-gray-600">
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              className="w-full border border-gray-300 rounded-md py-2 px-3"
+              value={updatedProduct.name}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="brand" className="block text-gray-600">
+              Brand Name
+            </label>
+            <input
+              type="text"
+              id="brand"
+              name="brand"
+              className="w-full border border-gray-300 rounded-md py-2 px-3"
+              value={updatedProduct.brand}
+              onChange={handleInputChange}
+              readOnly
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="type" className="block text-gray-600">
+              Type
+            </label>
+            <select
+              id="type"
+              name="type"
+              className="w-full border border-gray-300 rounded-md py-2 px-3"
+              value={updatedProduct.type}
+              onChange={handleInputChange}
+            >
+              <option value="">Select Type</option>
+              <option value="Sedan">Sedan</option>
+              <option value="SUV (Sports Utility Vehicle)">
+                SUV (Sports Utility Vehicle)
+              </option>
+              <option value="Sports Car">Sports Car</option>
+              {/* Add more options as needed */}
+            </select>
+          </div>
+          <div className="mb-4">
+            <label htmlFor="price" className="block text-gray-600">
+              Price
+            </label>
+            <input
+              type="text" // Change to text input
+              id="price"
+              name="price"
+              className="w-full border border-gray-300 rounded-md py-2 px-3"
+              value={updatedProduct.price}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="rating" className="block text-gray-600">
+              Rating
+            </label>
+            <input
+              type="number"
+              id="rating"
+              name="rating"
+              className="w-full border border-gray-300 rounded-md py-2 px-3"
+              value={updatedProduct.rating}
+              onChange={handleInputChange}
+            />
+          </div>
+          <button
+            type="submit"
+            className="bg-blue-600 text-white py-2 px-4 rounded-lg hover:bg-blue-700"
+          >
+            Update Product
+          </button>
+        </form>
         <Link to={`/`}>
           <button className="mt-4 bg-gray-400 text-white py-2 px-4 rounded-lg hover:bg-gray-500">
             Cancel
